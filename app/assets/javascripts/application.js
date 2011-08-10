e4ec1629cbec6a022db34a6c0268a2bb55513043
dagingaa <daginge@gmail.com>
2011-08-10 15:38:18 +0200

    Fixed assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// Remove later, turn into coffeescript
+$(document).ready(function() {
+    $(".hoverable").click(function() {
+      if($(this).children(".hide").length > 0) {
+      $(this).children(".hideable").hide();
+      $(this).children(".hideable").removeClass("hide");
+      $(this).children(".hideable").slideDown("fast");
+      }
+      else {
+      $(this).children(".hideable").slideUp("fast"); 
+      $(this).children(".hideable").addClass("hide");
+      }
+      });
+    $("#header").click(function() {
+      $(".fancy").addClass("rotate");
+      });
+    $("#whereami").click(function() {
+      get_position_data();
+      });
+    function get_position_data() {
+    position = navigator.geolocation.getCurrentPosition(position_manipulation);
+    }
+    function position_manipulation(position) {
+      var url = "http://maps.google.com/maps/api/staticmap?center="+position.coords.latitude+","+position.coords.longitude+"&zoom=15&size=275x200&maptype=roadmap&sensor=true";
+      if($("#position").hasClass("hide")) {
+        $("#position").hide();
+        $("#position").removeClass("hide");
+        $("#position").append("<img src='"+url+"' alt='Your position'>");
+        $("#position").slideDown("fast");
+      }
+      else {
+      }
+    }
+});
+
